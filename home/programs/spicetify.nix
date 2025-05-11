83b565e31d7a2a261c981df2f231ae6b7fd5ff66
spaceguy47-2 <santiagolanzas2009@gmail.com>
2025-05-10 17:17:34 -0700

    fixed spotify

--- home/programs/spicetify.nix
+++ home/programs/spicetify.nix
@@ -1,47 +1,29 @@
-{ lib, config, inputs, username, pkgs, ...}: {
+{ lib, config, pkgs, inputs, username, ... }:
 
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+in
+{
   imports = [
-    # Example for NixOS
-    spicetify-nix.homeManagerModules.spicetify 
+    inputs.spicetify-nix.homeManagerModules.spicetify
   ];
-  
-  let
-    # For Flakeless:
-    # spicePkgs = spicetify-nix.packages;
 
-  # With flakes:
-    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
-  in
-   
   programs.spicetify = {
-  enable = true;
+    enable = true;
+
     enabledExtensions = with spicePkgs.extensions; [
-      shuffle # shuffle+ (special characters are sanitized out of extension names)
+      shuffle
       beautifulLyrics
       oneko
-      
-      #cat jam
-      src = pkgs.fetchFromGitHub {
-        owner = "BlafKing";
-        repo = "github:BlafKing/spicetify-cat-jam-synced";
-        rev = "";
-        hash = "";
-      };
-      # The actual file name of the extension usually ends with .js
-      name = "cat-jam.js";
     ];
+
     theme = spicePkgs.themes.starryNight;
 
     enabledSnippets = [
-      {
-        "title": "Hide lyrics button",
-        "description": "Hides the lyrics button in the playbar",
-        "code": ".main-nowPlayingBar-lyricsButton { display: none; }",
-        "preview": "resources/assets/snippets/hide-lyrics-button.png"
-      },
-
+      ''
+        /* Hide lyrics button in playbar */
+        .main-nowPlayingBar-lyricsButton { display: none; }
+      ''
     ];
-
-  }
-
+  };
 }
